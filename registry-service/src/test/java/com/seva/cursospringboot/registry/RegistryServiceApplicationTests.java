2a068bc407f93badbcabedf7f19780979216e5b0
zeva369 <zeva77@gmail.com>
2023-06-13 17:36:34 +0200

    registry service added

---
+++ registry-service/src/test/java/com/seva/cursospringboot/registry/RegistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seva.cursospringboot.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
