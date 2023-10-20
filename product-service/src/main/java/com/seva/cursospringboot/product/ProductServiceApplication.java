cd582e648f9d2d6d3f04a3eb85a38e165fd6634a
zeva369 <zeva77@gmail.com>
2023-10-20 14:17:08 +0200

    guarde el archivo

--- product-service/src/main/java/com/seva/cursospringboot/product/ProductServiceApplication.java
+++ product-service/src/main/java/com/seva/cursospringboot/product/ProductServiceApplication.java
@@ -9,6 +9,6 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 public class ProductServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProductServiceApplication.class, args);
+		//ssfdsfs
 	}
-
 }
